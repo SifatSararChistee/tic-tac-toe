f7292f06203d9fca444e335cb2870c4aac2b7810
SIFAT <sifatsararchistee@gmail.com>
2025-12-15 21:53:23 +0600

    added the history time travel feature

--- src/App.jsx
+++ src/App.jsx
@@ -109,13 +109,35 @@ function calculateWinner(squares) {
 export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
 
-  const currentSquares = history[history.length - 1];
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
     setXIsNext(!xIsNext);
-    setHistory([...history, nextSquares]);
   }
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = "Go to move #" + move;
+    } else {
+      description = "Go to game start";
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
   return (
     <>
       <div>
@@ -126,7 +148,7 @@ export default function Game() {
         ></Board>
       </div>
       <div>
-        <ol></ol>
+        <ol>{moves}</ol>
       </div>
     </>
   );
